935095514aa246ecb209a29ffc2d1078b52efa4f
TashtandyevBaysal <nurlanibakov1@gmail.com>
2024-09-23 15:09:57 +0600

    Rating its ready

--- src/app/(Main)/(ui)/page.tsx
+++ src/app/(Main)/(ui)/page.tsx
@@ -2,6 +2,7 @@ import cls from "./page.module.scss";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import TourCard from "@/app/(Tours)/tour/(TourCurd)/TourCard";
 
 export default async function Home() {
 
@@ -12,6 +13,12 @@ export default async function Home() {
                     <h1>Авторские туры: Твое идеальное приключение</h1>
                 </span>
             </section>
+            <TourCard
+                selected={false}
+                rating={4.5}
+                id={"12"}
+                title={"Сары челек"}
+                image={"https://baktyiar.pythonanywhere.com/media/headline_img/dzhalal-abad-sary-chelek-1.webp"}/>
         </main>
     );
 }
