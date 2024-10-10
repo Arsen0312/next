594cf6ada132604a65cc9f1fb8b681e273952bbc
Arsen0312 <greenchay16@gmail.com>
2024-10-10 16:19:50 +0600

    Initial commit from Create Next App

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    images: {
-        domains: ['baktyiar.pythonanywhere.com'],
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
