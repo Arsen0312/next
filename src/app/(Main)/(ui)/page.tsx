eccd4b451bc9d2fbc27a27171ee20e5f89f5325c
Arsen0312 <greenchay16@gmail.com>
2024-10-17 17:15:55 +0600

    сохранение изменений

--- src/app/(Main)/(ui)/page.tsx
+++ src/app/(Main)/(ui)/page.tsx
@@ -2,8 +2,11 @@ import cls from "./page.module.scss";
 import SearchMainPage from "@/app/(Main)/(ui)/(SearchMainPage)/SearchMainPage";
 import SevenOblastsOfKyrgyzstan from "./SevenOblastsOfKyrgyzstan/ui/SevenOblastsOfKyrgyzstan";
 import AllTours from "@/app/(Main)/(ui)/(AllTours)/ui/AllTours";
+import TourCard from "@/app/(Tours)/tour/(TourCurd)/TourCard";
+import img from "@/assets/Osh.png"
 
 export default async function Home() {
+
 
     return (
         <main className={cls.main}>
@@ -17,6 +20,9 @@ export default async function Home() {
             </section>
             <SevenOblastsOfKyrgyzstan/>
             <AllTours/>
+            <TourCard id={0} title={"title"} description={"Тур-приключение: на внедорожниках к заброшенному городу Эныльчек"}
+                      image={img.src} rating={0.5} discount={50} price={12000}
+            />
         </main>
     );
 }
--- src/app/(Tours)/tour/(TourCurd)/TourCard.tsx
+++ src/app/(Tours)/tour/(TourCurd)/TourCard.tsx
@@ -1,8 +1,8 @@
 import cls from "./TourCard.module.scss"
 import Image from "next/image";
 import Link from "next/link";
-import discountIcon from "@/assets/discount.svg"
 import location from "@/assets/location.svg"
+import star from "@/assets/star.svg"
 
 type TTourCard = {
     id: number;
@@ -16,7 +16,7 @@ type TTourCard = {
 }
 
 const TourCard = (props: TTourCard) => {
-    const {title, rating, image, id, discount, description, price } = props
+    const {title, rating, image, id, description, price } = props
 
     return (
         <div className={cls.main}>
@@ -34,7 +34,10 @@ const TourCard = (props: TTourCard) => {
                             src={location} alt={"location"}/>
                         {title}
                     </h4>
-                    <h5>{rating}</h5>
+                    <h4>
+                        <Image width={24} height={24} src={star} alt={"star"}/>
+                        {rating}
+                    </h4>
                 </div>
                 <div className={cls.description}>
                     <p>
@@ -42,21 +45,8 @@ const TourCard = (props: TTourCard) => {
                     </p>
                 </div>
                 <div className={cls.priceWithOrderTour}>
-                    <span>от {price} сом</span>
-                    <button>
-                        Заказать
-                    </button>
+                    <span>{price}сом за одного</span>
                 </div>
-                {
-                    typeof discount === "number"
-                        ? (<div className={cls.wrapperDiscountIcon}>
-                            <div className={cls.wrapperForPositionDiscount}>
-                                <Image width={1000} height={1000} src={discountIcon} alt={"скидка"}/>
-                                <span>{discount}%</span>
-                            </div>
-                        </div>)
-                        : <></>
-                }
             </Link>
 
         </div>
